1d045e97f5196bbfc3e4075b4cbc25ccac888c86
un57gcder1 <un57gcder1@gmail.com>
2025-02-09 15:31:22 -0500

    Added encode and decode function

--- data.py
+++ data.py
@@ -47,18 +47,22 @@ class TextDataLoader:
         with open(self.train, 'r') as f:
             for line in f:
                 for w in line:
-                    self.training_data.append(self.tokens[w])
+                    la_list = [0]*self.size()
+                    la_list[self.tokens[w]] = 1
+                    self.training_data.append(la_list)
         with open(self.valid, 'r') as f:
             for line in f:
                 for w in line:
+                    la_list = [0]*self.size()
                     # Accounting for unknown tokens
                     try:
-                        self.valid_data.append(self.tokens[w])
+                        la_list[self.tokens[w]] = 1
                     except:
-                        self.valid_data.append(self.tokens["unk"])
+                        la_list[self.tokens["unk"]] = 1
+                    self.valid_data.append(la_list)
 
-        self.training_data = tensor(self.training_data)
-        self.valid_data = tensor(self.valid_data)
+        self.training_data = tensor(self.training_data, dtype=torch.float32)
+        self.valid_data = tensor(self.valid_data, dtype=torch.float32)
         return (self.training_data, self.valid_data)
 
     def substantiate(self, window = 1):
@@ -78,19 +82,26 @@ class TextDataLoader:
 
         return (self.training_data, self.valid_data)
 
-    # Batch size (default is 8 tokens)
+    # Batch size (default is 1 token)
     # Note: Removes last, incomplete batch (no padding)
-    def batch(self, batch_size = 8):
-        if self.training_data.dim() == 2 and self.valid_data.dim() == 2:
-            self.training_data = self.training_data[:,:(self.training_data.size()[1]//batch_size)*batch_size]
-            self.training_data = self.training_data.view(self.training_data.size()[0], 
-                               self.training_data.size()[1]//batch_size, 
-                               batch_size)
+    def batch(self, batch_size = 1):
+        if self.training_data.dim() == 3 and self.valid_data.dim() == 3:
+            # Rounds down to remove last batch: ex. [2, 108, 37] --> [2, 104, 37]
+            self.training_data = self.training_data[:,:(self.training_data.size()[1]//batch_size)*batch_size,:]
 
-            self.valid_data = self.valid_data[:,:(self.valid_data.size()[1]//batch_size)*batch_size] 
-            self.valid_data = self.valid_data.view(self.valid_data.size()[0], 
-                               self.valid_data.size()[1]//batch_size, 
-                               batch_size)
+            # Format: 2 variables, num_batches, batch_size, vocab_size
+            self.training_data = self.training_data.view(self.training_data.size()[0],
+                               self.training_data.size()[1]//batch_size, batch_size,
+                               self.training_data.size()[2])
+
+            # Rounds down to remove last batch: ex. [2, 108, 37] --> [2, 104, 37]
+            self.valid_data = self.valid_data[:,:(self.valid_data.size()[1]//batch_size)*batch_size, :]
+
+            self.valid_data = self.valid_data.view(self.valid_data.size()[0],
+                               self.valid_data.size()[1]//batch_size, batch_size,
+                                                   self.valid_data.size()[2])
+        return (self.training_data, self.valid_data)
+        """
         elif self.training_data.dim() == 1 and self.valid_Data.dim() == 1: 
             self.training_data = self.training_data[:(self.training_data.size()[0]//batch_size)*batch_size]
             self.training_data = self.training_data.view(self.training_data.size()[1]//batch_size, 
@@ -99,8 +110,32 @@ class TextDataLoader:
             self.valid_data = self.valid_data[:(self.valid_data.size()[0]//batch_size)*batch_size]
             self.valid_data = self.valid_data.view(self.valid_data.size()[1]//batch_size, 
                                batch_size)
-        else:
-            raise Exception("Training Data & Valid Data tensors are of wrong shape.")
-        return (self.training_data, self.valid_data)
+        """
+    """else:
+            raise Exception("Training Data & Valid Data tensors are of wrong shape.")"""
+        #return (self.training_data, self.valid_data)
+
     def size(self):
         return len(self.tokens)
+
+    def encode(self, theInput):
+        output = []
+        for i in theInput:
+            la_list = [0]*self.size()
+            # Accounting for unknown tokens
+            try:
+                la_list[self.tokens[i]] = 1
+            except:
+                la_list[self.tokens["unk"]] = 1
+            output.append(la_list)
+        output = tensor(output, dtype=torch.float32)
+        return output
+
+    def decode(self, output): 
+        decoder = {v: k for k,v in self.tokens.items()}
+        theInput = ""
+        for i in output:
+            value, index = torch.max(i, dim=0)
+            num = index.item()
+            theInput += decoder[num]
+        return theInput
