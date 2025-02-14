101be5df3f4750bc6e847af99f471eca9a91a070
un57gcder1 <un57gcder1@gmail.com>
2025-02-13 22:18:25 -0500

    Working

--- t.py
+++ t.py
@@ -12,29 +12,21 @@ CHARS = 200
 
 t = TextDataLoader(["testing/smallt.txt","testing/smallv.txt"], WINDOW)
 
-l = t.tokenize()
+#l = t.tokenize()
 
-m = t.numericalize()
+#m = t.numericalize()
 
-print(m[0].shape, m[1].shape)
+#print(m[0].shape, m[1].shape)
 
-n = t.substantiate()
+#n = t.substantiate()
 
-print(n[0][0].shape, n[0][1].shape, n[1][0].shape, n[1][1].shape)
+#print(n[0][0].shape, n[0][1].shape, n[1][0].shape, n[1][1].shape)
 
-t.save()
+#t.save()
 
-#o = t.load()
-"""
-print(m[0].shape, m[1].shape)
+o = t.load()
 
-print(n[0].shape, n[1].shape)
-
-print(o[0].shape, o[1].shape)
-
-print(o[0], o[1])
-"""
-#model = RNN(t.size(), HS, BS, o[0], o[1])
-#model.load()
-#s = model.train(learning_rate=LR, generate=True, epochs=EPOCHS, tdl=t, prompt=PROMPT, window=WINDOW, temperature=TEMP)
-#model.save("50-epochs.pth")
+model = RNN(t.size(), HS, WINDOW, o[0], o[1])
+model.load()
+s = model.train(learning_rate=LR, generate=True, epochs=EPOCHS, tdl=t, prompt=PROMPT, temperature=TEMP)
+model.save("100-epochs.pth")
