53268149a747b1642bc99001f3d7e16c661a63b5
un57gcder1 <un57gcder1@gmail.com>
2025-02-24 17:30:45 -0500

    Finished redoing data file for new embedding approach

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ NOTE: Data will not be one-hot encoded due to memory constraints; change in prog
 
 """
 class TextDataLoader:
-    def __init__(self, textFile, window):
+    def __init__(self, textFile):
         if type(textFile) == list and len(textFile) == 2:
             self.train = textFile[0]
             self.valid = textFile[1]
@@ -33,14 +33,8 @@ class TextDataLoader:
             raise Exception("Validation file does not have a valid path.")
 
         self.tokens = {"unk": 0}
-        self.decoder = None
         self.training_data = []
         self.valid_data = []
-        self.window = window 
-        if self.window < 1:
-            raise Exception("Window size must be greater than or equal to 1.")
-        if self.window > 1000:
-            raise Warning("Window size is unusually large. May cause errors.")
 
     # Reads file and tokenizes it, along with assigning a numerical value
     def tokenize(self):
@@ -49,109 +43,77 @@ class TextDataLoader:
                 for w in line:
                     if w not in self.tokens:
                         self.tokens[w] = len(self.tokens)
-        self.decoder = {v: k for k,v in self.tokens.items()}
-        return self.tokens
 
     def numericalize(self):
         with open(self.train, 'r') as f:
             for line in f:
                 for w in line:
-                    la_list = [0]*self.size()
-                    la_list[self.tokens[w]] = 1
-                    self.training_data.append(la_list)
+                    self.training_data.append(self.tokens[w])
         with open(self.valid, 'r') as f:
             for line in f:
                 for w in line:
-                    la_list = [0]*self.size()
                     # Accounting for unknown tokens
                     try:
-                        la_list[self.tokens[w]] = 1
+                        self.valid_data.append(self.tokens[w])
                     except:
-                        la_list[self.tokens["unk"]] = 1
-                    self.valid_data.append(la_list)
-
+                        self.valid_data.append(self.tokens["unk"])
         self.training_data = tensor(self.training_data, dtype=torch.float32)
         self.valid_data = tensor(self.valid_data, dtype=torch.float32)
-        return (self.training_data, self.valid_data)
 
-    def substantiate(self):
-        ind_var = self.training_data[:len(self.training_data)-1]
-        ind_var = self.__expand(ind_var, ind_var.size()[0])
-        dep_var = self.training_data[self.window:]
-        dep_var = dep_var.view(dep_var.size()[0], 1, dep_var.size()[1])
-        self.training_data = (ind_var, dep_var)
+    # Dividing into two x, y tuples for training and valid data
+    def divide(self):
+        x_data = self.training_data
+        y_data = torch.roll(self.training_data, shifts = 1, dims = 0)
+        self.training_data = (x_data, y_data)
+        xval_data = self.valid_data
+        yval_data = torch.roll(self.valid_data, shifts = 1, dims = 0)
+        self.valid_data = (xval_data, yval_data)
 
-        ind_val_var = self.valid_data[:len(self.valid_data)-1]
-        ind_val_var = self.__expand(ind_val_var, ind_val_var.size()[0])
-        dep_val_var = self.valid_data[self.window:]
-        dep_val_var = dep_val_var.view(dep_val_var.size()[0], 1, dep_val_var.size()[1])
-        self.valid_data = (ind_val_var, dep_val_var)
+    # Right now: divide() must be run before substantiate()
+    def substantiate(self, batch_size, timesteps):
+        factor = batch_size * timesteps
+        x_data = self.training_data[0]
+        y_data = self.training_data[1]
 
-        return (self.training_data, self.valid_data)
+        x_data = x_data[:x_data.size()[0]-(x_data.size()[0] % timesteps)]
+        y_data = y_data[:y_data.size()[0]-(y_data.size()[0] % timesteps)]
 
-    # Expanding the independent variable to window size
-    # theTensor = shape [examples, vocab_size]
-    # Outputs [examples, window_size, vocab_size]
-    # Mem = aimed at reducing memory usage (SOMETHING BETTER IS NEEDED!)
-    def __expand(self, theTensor, examples, mem = 10000):
-        nL = []
-        laTensors = []
-        for i in range(examples):
-            unique = theTensor[0+i:self.window+i,:]
-            if (unique.size()[0] != self.window):
-                break
-            nL.append(unique)
-            if i % mem == 0:
-                print(i)
-                laTensors.append(torch.stack(nL))
-                nL = []
-        laTensors.append(torch.stack(nL))
-        out = torch.cat(laTensors, dim=0)
-        return out
+        x_data = x_data.view(batch_size, timesteps)
+        y_data = y_data.view(batch_size, timesteps)
 
-    def size(self):
-        return len(self.tokens)
+        xval_data = self.valid_data[0]
+        yval_data = self.valid_data[1]
 
-    def encode(self, theInput):
-        output = []
-        for i in theInput:
-            la_list = [0]*self.size()
-            # Accounting for unknown tokens
-            try:
-                la_list[self.tokens[i]] = 1
-            except:
-                la_list[self.tokens["unk"]] = 1
-            output.append(la_list)
-        output = tensor(output, dtype=torch.float32)
-        return output
+        xval_data = xval_data[:xval_data.size()[0]-(xval_data.size()[0] % timesteps)]
+        yval_data = yval_data[:yval_data.size()[0]-(yval_data.size()[0] % timesteps)]
 
-    def decode(self, output, sampling = "multinomial"): 
-        theInput = ""
-        for i in output:
-            num = self.__sample(i, sampling)
-            theInput += self.decoder[num]
-        return theInput
-    
-    def save(self, filename="db.pth"):
+        xval_data = xval_data.view(batch_size, timesteps)
+        yval_data = yval_data.view(batch_size, timesteps)
+
+        self.training_data = (x_data, y_data)
+        self.valid_data = (xval_data, yval_data)
+
+    def save(self, filename="db.pth", t="tokens.pth"):
         d = {"training_data":self.training_data,
-             "valid_data":self.valid_data,
-             "tokens":self.tokens}
+             "valid_data":self.valid_data}
         torch.save(d, filename)
+        torch.save(self.tokens, t)
         return 0
 
-    def load(self, filename="db.pth"):
+    def load(self, filename="db.pth", t="tokens.pth"):
         d = torch.load(filename)
         self.training_data = d["training_data"]
         self.valid_data = d["valid_data"]
-        self.tokens = d["tokens"]
-        return (self.training_data, self.valid_data)
+        self.tokens = torch.load(t)
+        return 0
 
-    def __sample(self, theTensor, sampling):
-        if sampling == "multinomial":
-            num = random.choices(list(self.decoder.keys()), weights=i, k=1)[0]
-        elif sampling == "greedy":
-            val, ind = torch.max(i, dim=0)
-            num = ind.item()
-        else:
-            raise Exception("Sampling method must be str: multinomial, greedy.")
-        return num
+    def preprocess(self, batch_size, timesteps, save=True, **kwargs):
+        self.tokenize()
+        self.numericalize()
+        self.divide()
+        self.substantiate(batch_size, timesteps)
+        if save:
+            t = kwargs.get("t", "tokens.pth")
+            filename = kwargs.get("filename", "db.pth")
+            self.save(filename, t)
+        return 0
